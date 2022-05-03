07d7b98cdaeb3bbfd3dad63f0e3ebce94392b757
oyerottiii <rr20152107@gmail.com>
2022-05-03 13:11:02 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+          child: Center(
+        child: Text("Hey there!"),
+      )),
+    );
+  }
+}
